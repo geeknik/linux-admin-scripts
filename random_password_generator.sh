a426364ac9e1faaff14d07062cc86039b8a0304e
geeknik <466878+geeknik@users.noreply.github.com>
2023-06-24 04:37:48 +0000

    Update random_password_generator.sh

--- random_password_generator.sh
+++ random_password_generator.sh
@@ -2,4 +2,4 @@
 PASS_LEN=30
 PASSWORD=$(head /dev/urandom | tr -dc 'A-Za-z0-9!@#$%^&*()-=_+[]{}|;:,.<>?/~`' | fold -w ${PASS_LEN} | head -n 1)
 PASSWORD=$(echo $PASSWORD | sed 's/\(.\)/\1\n/g' | awk '{ if (rand() > 0.5) print toupper($0); else print tolower($0); }' ORS='')
-echo "Your random password is: $PASSWORD" # generate random password
+echo "Your random password is: $PASSWORD" # generate robust random password
